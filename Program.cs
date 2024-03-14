f94e2931399c232409454e2ce033a656fbde9619
Brian Ackerman <backer0285@gmail.com>
2024-03-14 13:16:39 -0500

    [1.10] Initial

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using NLog;
+
+// See https://aka.ms/new-console-template for more information
+string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+
+// create instance of Logger
+var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
+logger.Info("Program started");
+
+string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
+logger.Info(scrubbedFile);
+MovieFile movieFile = new MovieFile(scrubbedFile);
+
+logger.Info("Program ended");
