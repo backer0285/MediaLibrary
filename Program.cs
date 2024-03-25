4d9c25d063aee7177db3c048e039f7c849e2f806
Brian Ackerman <backer0285@gmail.com>
2024-03-25 16:40:52 -0500

    [1.12] Display Movies

--- Program.cs
+++ Program.cs
@@ -14,21 +14,25 @@ MovieFile movieFile = new MovieFile(scrubbedFile);
 string choice = "";
 do
 {
-  // display choices to user
-  Console.WriteLine("1) Add Movie");
-  Console.WriteLine("2) Display All Movies");
-  Console.WriteLine("Enter to quit");
-  // input selection
-  choice = Console.ReadLine();
-  logger.Info("User choice: {Choice}", choice);
-  if (choice == "1")
-  {
-    // Add movie
-  } 
-  else if (choice == "2")
-  {
-    // Display All Movies
-  }
+    // display choices to user
+    Console.WriteLine("1) Add Movie");
+    Console.WriteLine("2) Display All Movies");
+    Console.WriteLine("Enter to quit");
+    // input selection
+    choice = Console.ReadLine();
+    logger.Info("User choice: {Choice}", choice);
+    if (choice == "1")
+    {
+        // Add movie
+    }
+    else if (choice == "2")
+    {
+        // Display All Movies
+        foreach (Movie m in movieFile.Movies)
+        {
+            Console.WriteLine(m.Display());
+        }
+    }
 } while (choice == "1" || choice == "2");
 
 logger.Info("Program ended");
